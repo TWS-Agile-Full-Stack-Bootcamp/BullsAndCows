550aec87012b88a85c5cf4b3bcd3d7416398f95b
Wu Xueheng <xhwu@thoughtworks.com>
2020-11-05 21:27:16 +0800

    feat: Should_return_true_when_ValidateGuess_given_4_different_numbers

---
+++ BullsAndCows/BullsAndCowsGame.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace BullsAndCows
+{
+    public class BullsAndCowsGame
+    {
+        public BullsAndCowsGame()
+        {
+        }
+
+        public bool ValidateGuess(string guess)
+        {
+            return true;
+        }
+    }
+}
---
+++ BullsAndCowsTest/BullsAndCowsGameTest.cs
@@ -0,0 +1,22 @@
+namespace BullsAndCowsTest
+{
+    using BullsAndCows;
+    using Xunit;
+
+    public class BullsAndCowsGameTest
+    {
+        [Fact]
+        public void Should_return_true_when_ValidateGuess_given_4_different_numbers()
+        {
+            // given
+            string guess = "1 2 3 4";
+
+            // when
+            BullsAndCowsGame game = new BullsAndCowsGame();
+            bool isValid = game.ValidateGuess(guess);
+
+            // then
+            Assert.True(isValid);
+        }
+    }
+}
