31a21e0071787b40eb168620e473fe3a64478352
Wu Xueheng <xhwu@thoughtworks.com>
2020-11-05 20:13:46 +0800

    feat: Should_random_when_generate_answer

--- BullsAndCows/AnswerGenerator.cs
+++ BullsAndCows/AnswerGenerator.cs
@@ -5,13 +5,29 @@ namespace BullsAndCows
 {
     public class AnswerGenerator
     {
+        private const int ANSWER_NUMBER_COUNT = 4;
+        private readonly Random random;
+
         public AnswerGenerator(Random random)
         {
+            this.random = random;
         }
 
         public List<int> Generate()
         {
-            return new List<int> { 1, 2, 3, 4 };
+            List<int> answer = new List<int>();
+            int numberCount = 0;
+            while (numberCount < ANSWER_NUMBER_COUNT)
+            {
+                int randomNumber = random.Next(0, 9);
+                if (!answer.Contains(randomNumber))
+                {
+                    answer.Add(randomNumber);
+                    numberCount++;
+                }
+            }
+
+            return answer;
         }
     }
 }
--- BullsAndCowsTest/AnswerGeneratorTest.cs
+++ BullsAndCowsTest/AnswerGeneratorTest.cs
@@ -20,5 +20,20 @@ namespace BullsAndCowsTest
             // then
             Assert.Equal(4, answer.Count());
         }
+
+        [Fact]
+        public void Should_random_when_generate_answer()
+        {
+            // given
+            AnswerGenerator answerGenerator = new AnswerGenerator(new Random());
+
+            // when
+            List<int> answer1 = answerGenerator.Generate();
+            List<int> answer2 = answerGenerator.Generate();
+            List<int> answer3 = answerGenerator.Generate();
+
+            // then
+            Assert.False(answer1.SequenceEqual(answer2) && answer2.SequenceEqual(answer3));
+        }
     }
 }
