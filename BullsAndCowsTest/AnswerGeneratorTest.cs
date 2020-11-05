1f415e76c8eec4095b1120a183cc6d8604a21ae2
Wu Xueheng <xhwu@thoughtworks.com>
2020-11-05 20:34:03 +0800

    test: Should_return_no_duplicate_number_when_generate_answer

--- BullsAndCowsTest/AnswerGeneratorTest.cs
+++ BullsAndCowsTest/AnswerGeneratorTest.cs
@@ -4,6 +4,7 @@ namespace BullsAndCowsTest
     using System.Collections.Generic;
     using System.Linq;
     using BullsAndCows;
+    using Moq;
     using Xunit;
 
     public class AnswerGeneratorTest
@@ -34,6 +35,22 @@ namespace BullsAndCowsTest
 
             // then
             Assert.False(answer1.SequenceEqual(answer2) && answer2.SequenceEqual(answer3));
+        }
+
+        [Fact]
+        public void Should_return_no_duplicate_number_when_generate_answer()
+        {
+            // given
+            var mock = new Mock<Random>();
+            mock.SetupSequence(random => random.Next(It.IsAny<int>(), It.IsAny<int>()))
+                .Returns(1).Returns(1).Returns(2).Returns(3).Returns(1).Returns(2).Returns(3).Returns(4);
+            AnswerGenerator answerGenerator = new AnswerGenerator(mock.Object);
+
+            // when
+            List<int> answer = answerGenerator.Generate();
+
+            // then
+            Assert.Equal(4, answer.Distinct().Count());
         }
     }
 }
