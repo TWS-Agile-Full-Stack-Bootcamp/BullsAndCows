cca15a51aee543d1f2c2a7ee7a11c910f05f21ae
ChenXu <xuchen@thoughtworks.com>
2020-11-13 01:08:28 +0800

    feat: more test cases for input should be digit between space

--- BullsAndCows/BullsAndCowsGame.cs
+++ BullsAndCows/BullsAndCowsGame.cs
@@ -4,17 +4,33 @@ namespace BullsAndCows
 {
     public class BullsAndCowsGame
     {
+        private const string WrongInputMessage = "Wrong Input, input again";
+
         public bool CanContinue { get; private set; }
 
         public string Judge(string input)
         {
-            var splitedDigits = input.Split(' ');
-            if (splitedDigits.Length != 4)
+            if (IsInputDigitBetweenSpace(input, out var wrongInputInputAgain))
             {
-                return "Wrong Input, input again";
+                return wrongInputInputAgain;
             }
 
             return string.Empty;
+        }
+
+        private static bool IsInputDigitBetweenSpace(string input, out string wrongInputInputAgain)
+        {
+            wrongInputInputAgain = string.Empty;
+            var splitedDigits = input.Split(' ');
+            if (splitedDigits.Length != 4)
+            {
+                {
+                    wrongInputInputAgain = WrongInputMessage;
+                    return true;
+                }
+            }
+
+            return false;
         }
     }
 }
--- BullsAndCowsTest/BullsAndCowsGameTest.cs
+++ BullsAndCowsTest/BullsAndCowsGameTest.cs
@@ -12,11 +12,13 @@ namespace BullsAndCowsTest
             Assert.NotNull(game);
         }
 
-        [Fact]
-        public void Should_return_wrong_input_message_when_input_not_between_space()
+        [Theory]
+        [InlineData("1234")]
+        [InlineData("12 3 4")]
+        [InlineData("12 34")]
+        public void Should_return_wrong_input_message_when_input_not_between_space(string input)
         {
             var game = new BullsAndCowsGame();
-            var input = "1234";
             var output = game.Judge(input);
             Assert.Equal("Wrong Input, input again", output);
         }
